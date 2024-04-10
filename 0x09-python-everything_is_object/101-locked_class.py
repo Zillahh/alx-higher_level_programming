f646841b5483a6dd9dfbcbc153595c5ad951a92e
Zillahh <ugwubonifaceotu1998@gmail.com>
2024-04-09 17:19:33 -0700

    attr

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+#work by: ugwu boniface otu
+"""A class with no class or object attribute
+
+that prevents user: from creating new instance attribute
+
+except: if the new instance attribute is called first_name
+
+"""
 
 class LockedClass:
     """A class that prevents instance except instance with the attribute first_name
