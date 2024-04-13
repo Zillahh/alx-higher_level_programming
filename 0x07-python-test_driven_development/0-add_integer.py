5d658dd9588480940b8ac3f443f7c5775fb91dca
Zillahh <ugwubonifaceotu1998@gmail.com>
2024-04-12 18:02:32 -0700

    Test passed

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-""" a function that computes the addition of two ints"""
-
-
 def add_integer(a, b=98):
-    """it returns the integer addition of a and b """
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
-    if type(a) == float:
+    if isinstance(a, float):
         a = int(a)
-    if type(b) == float:
+    if isinstance(b, float):
         b = int(b)
-    return (int(a) + int(b))
+    return (a + b)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-""" a function that multiplies 2 matrices by using the module NumPy"""
-import numpy as np
-""" the numerical python module called numpy that counts numbers faster"""
+"""
+lazy matrix math
+"""
+
+import numpy
 
 
 def lazy_matrix_mul(m_a, m_b):
-    """ a function that multiplies 2 matrices"""
-    return np.matmul(m_a, m_b)
+    """calculates multiplication"""
+    return numpy.matmul(m_a, m_b)
